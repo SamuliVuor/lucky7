b686aed0f53b9a047f7e59d071534e0f8bc266b2
SamuliVuor <samuli23006@student.hamk.fi>
2023-09-04 12:45:29 +0300

    Ekat lisäykset/Muutokset
    Lisäsin raha systeemin peliin, jota käytetään pelaamiseen ja jota voi voittaa.  Pelin päättyminen pitää korjata.

--- src/lucky7/lucky7harjoitus.java
+++ src/lucky7/lucky7harjoitus.java
@@ -1,26 +1,69 @@
 package lucky7;
 import java.util.Random;
+import java.util.Scanner;
 public class lucky7harjoitus {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub Do while
 		Random random = new Random();
+		Scanner in = new Scanner(System.in);
 		
+
+		int raha = 1;
+		String vastaus;
+		
+		do {
+		System.out.println("Haluatko pelata? y/n");
+		vastaus = in.nextLine();
 		int random1 = random.nextInt(10 - 1 + 1) + 1 ;
 		int random2 = random.nextInt(10 - 1 + 1) + 1 ;
 		int random3 = random.nextInt(10 - 1 + 1) + 1 ;
-		
-		System.out.println(random1);
-		System.out.println(random2);
-		System.out.println(random3);
-		
+	
+			if(vastaus.equals("y"))	{	
+				System.out.println(random1);
+				System.out.println(random2);
+				System.out.println(random3);
 		if (random1 == 7 || random2 == 7 || random3 == 7) {
-			System.out.println("Voitit pelin!");
+			System.out.println("Voitit 3 euroa!");
+			raha+=3;
+			
+		}
+		else if (random1 == 7 && random2 == 7) {
+			System.out.println("Voitit 5 euroa");	
+			raha+=5;
+		}
+		else if (random1 == 7 && random3 == 7) {
+			System.out.println("Voitit 5 euroa");
+			raha+=5;
+		}
+		else if (random2 == 7 && random3 == 7) {
+			System.out.println("Voitit 5 euroa");
+			raha+=5;
+			
+		}
+		else if (random1 == 7 && random2 == 7 && random3 == 7) {
+			System.out.println("Voitit 10 euroa");
+			raha+=10;
+			
 		}
 		else {
-			System.out.println("Hävisit pelin");
+			System.out.println("Hävisit euron");
+			raha--;
+		}
+			}
+			else if (vastaus.equals("n")) {
+				System.out.println("Lopetetaan peli, sinulle jäi " + raha + " euroa");
+			}
+			else 
+			System.out.println("Virhe! Vastaa y jos haluat jatkaa pelia ja vastaa n jos haluat lopettaa pelin");
+		} while (raha >= 0 || !vastaus.equals("n")); {
+		if (raha == 0) {
+			System.out.println("Sinulta loppui rahat!");
+		}
 		}
 		
+		}
+	
 	}
 
-}
+
