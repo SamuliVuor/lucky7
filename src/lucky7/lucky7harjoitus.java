9104f55118791f166e4f0a097d5a6f23e71b5d7a
SamuliVuor <samuli23006@student.hamk.fi>
2023-09-06 10:33:07 +0300

    kolmas(viimeinen?) commit
    Lisäsin käyttäjän itse pystymään päättämään rahan määrän millä aloittaa pelin. Korjasin myös, että voitot maksavat oikean summan. Myös pieniä parannuksia muussa koodissa.

--- src/lucky7/lucky7harjoitus.java
+++ src/lucky7/lucky7harjoitus.java
@@ -9,55 +9,90 @@ public class lucky7harjoitus {
 		Scanner in = new Scanner(System.in);
 		
 
-		int raha = 5;
+		String raha;
+		int nraha;
+		boolean tryagain;
+		
+		do { System.out.println("Millä raha määrällä haluat pelata?");
+	raha = in.nextLine(); 
+	 if (raha.matches("[0-9]*")){
+		 tryagain = false;
+	 }
+	 else {
+		 tryagain = true;
+		 System.out.println("Virhe! Anna numero!");
+	 }
+	
+		} while (tryagain);
+	
+	nraha = Integer.parseInt(raha);
 		String vastaus;
 		
+		
+		
 		do {
+			
+	if (nraha == 0) {
+		break;
+	}
+	
 		System.out.println("Haluatko pelata? y/n");
 		vastaus = in.nextLine();
 		int random1 = random.nextInt(10 - 1 + 1) + 1 ;
 		int random2 = random.nextInt(10 - 1 + 1) + 1 ;
 		int random3 = random.nextInt(10 - 1 + 1) + 1 ;
+				 //random.nextInt(10 - 1 + 1) + 1 ;
 	
 			if(vastaus.equals("y"))	{	
 				System.out.println(random1);
 				System.out.println(random2);
 				System.out.println(random3);
-		if (random1 == 7 || random2 == 7 || random3 == 7) {
-			System.out.println("Voitit 3 euroa!");
-			raha+=3;
 			
-		}
+			if (random1 == 7 && random2 == 7 && random3 == 7) {
+					System.out.println("Voitit 10 euroa");
+					nraha+=10;
+					continue;
+				 }
 		else if (random1 == 7 && random2 == 7) {
 			System.out.println("Voitit 5 euroa");	
-			raha+=5;
+			nraha+=5;
+			continue;
 		}
 		else if (random1 == 7 && random3 == 7) {
 			System.out.println("Voitit 5 euroa");
-			raha+=5;
+			nraha+=5;
+			continue;
 		}
 		else if (random2 == 7 && random3 == 7) {
 			System.out.println("Voitit 5 euroa");
-			raha+=5;
+			nraha+=5;
+			continue;
 			
 		}
-		else if (random1 == 7 && random2 == 7 && random3 == 7) {
-			System.out.println("Voitit 10 euroa");
-			raha+=10;
+
+		else if (random1 == 7 || random2 == 7 || random3 == 7) {
+				System.out.println("Voitit 3 euroa!");
+				nraha+=3;
 			
 		}
 		else {
 			System.out.println("Hävisit euron");
-			raha--;
+			nraha--;
+				
+		
 		}
-			}
+
+		}
+		
+
+			
 			else if (vastaus.equals("n")) {
-				System.out.println("Lopetetaan peli, sinulle jäi " + raha + " euroa.");
+				System.out.println("Lopetetaan peli, sinulle jäi " + nraha + " euroa.");
 			}
 			else 
 			System.out.println("Virhe! Vastaa y jos haluat jatkaa pelia ja vastaa n jos haluat lopettaa pelin");
-		} while (raha != 0 && !vastaus.equals("n")); {
-		if (raha == 0) {
+		} while (!vastaus.equals("n")); {
+		if (nraha == 0) {
 			System.out.println("Sinulta loppui rahat!");
 		}
 		}
